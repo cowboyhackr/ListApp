57dc40fde14a6d7ce132a0efb81de279d38642f1
System Administrator <john.m.mckenna@live.com>
2017-06-26 21:38:30 -0500

    two views

--- index.ios.js
+++ index.ios.js
@@ -2,20 +2,50 @@ import React from 'react';
 import {
   AppRegistry,
   Text,
+  View,
+  Button
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: 'Welcome',
+    title: 'Home',
   };
   render() {
-    return <Text>Hello, Navigation index ios!</Text>;
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Home App!</Text>
+        <Button
+          onPress={() => navigate('List')}
+          title="List"
+        />
+      </View>
+      );
+  }
+}
+
+class ListScreen extends React.Component {
+  static navigationOptions = {
+    title: 'List',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, LIST App!</Text>
+        <Button
+          onPress={() => navigate('Home')}
+          title="Home"
+        />
+      </View>
+      );
   }
 }
 
 const ListApp = StackNavigator({
   Home: { screen: HomeScreen },
+  List: { screen: ListScreen },
 });
 
 AppRegistry.registerComponent('ListApp', () => ListApp);
